1ae9ba786afbfcff3adc0c02d55ab85eaa54e08e
Gnxela <qawswedfrgtt@gmail.com>
2016-08-02 14:46:46 +0100

    Changed JS heavey color changer to a more CSS based approch in '/'.

--- main.js
+++ main.js
@@ -1,28 +1,10 @@
 $(document).ready(function() {
-	var fps = 30;
-	var frames = 0;
-	var changeTime = 150;//Change time in frames
-	var ored = 255, ogreen = 250, oblue = 255;//Original values
-	var red = ored, green = ogreen, blue = oblue;//Current values
-	var tred = 25, tgreen = 100, tblue = 255;//Target values
-
-	var myFunction = function() {
-		if(frames == changeTime) {
-			frames = 0;
-			ored = tred;
-			ogreen = tgreen;
-			oblue = tblue;
-			tred = Math.floor(Math.random() * 256);
-			tgreen = Math.floor(Math.random() * 256);
-			tblue = Math.floor(Math.random() * 256);
-		} else {
-			frames++;
-			red = red + (tred - ored) / changeTime;
-			green = green + (tgreen - ogreen) / changeTime;
-			blue = blue + (tblue - oblue) / changeTime;
-			$('.colorChange').css("backgroundColor", "rgb(" + Math.round(red) + ", " + Math.round(green) + ", " + Math.round(blue) + ")");
-			$('.colorChange2').css("color", "rgb(" + Math.round(red) + ", " + Math.round(green) + ", " + Math.round(blue) + ")");
-		}
-	};
-	setInterval(myFunction, (1000 / fps));
+	setInterval(colorChange, 5000);
 });
+function colorChange() {
+        var color = "rgb(" + Math.round(Math.random() * 256) + ", " + Math.round(Math.random() * 256) + ", " + Math.round(Math.random() * 256) + ")";
+        $('.colorChange').css("backgroundColor", color);
+        $('.colorChange2').css("color", color);
+	console.log(1);
+}
+
