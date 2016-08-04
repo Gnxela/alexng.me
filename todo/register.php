14c4f356a1c6c315dee7c918a538ca821110c4a0
Gnxela <qawswedfrgtt@gmail.com>
2016-08-04 15:40:06 +0100

    Fixed <form> and worked on registration in 'todo/'.

--- todo/register.php
+++ todo/register.php
@@ -5,7 +5,33 @@ $database = new Database();
 $database -> readConfig();
 $database -> connect();
 
-var_dump($_POST);
+$error = "&nbsp;";//Non breaking space
+
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+	do {
+		if(!isSet($_POST['username']) && !isSet($_POST['password']) && !isSet($_POST['password2'])) {
+			$error =  "Malformed request.";
+			break;
+		}
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$password2 = $_POST['password2'];
+		if(strlen($username) < 4) {
+			$error = "Username must be at least 4 characters long.";
+			break;
+		}
+		if(strlen($password) < 6) {
+			$error = "Password must be at least 6 characters long.";
+			break;
+		}
+		if($password != $password2) {
+			$error = "Passwords do not match.";
+			break;
+		}
+
+		//SUCCESS - Continue registering!
+	} while(0);//Will run once, and can be broken out of.
+}
 ?>
 <head>
 	<link rel="stylesheet" type="text/css" href="index.css">
@@ -17,13 +43,14 @@ var_dump($_POST);
 		Register
 	</div>
 	<div class="login"><a href="login.php">login</a> / <a href="register.php">register</a></div>
-	<form method="post" action="register.php" align="center">
-		<div class="title" name="username">Username</div>
-		<input type="text"></input><br>
-		<div class="title" name="password">Password</div>
-		<input type="password"></input>
-		<div class="title" name="password2">Confirm Password</div>
-		<input type="password"></input><br><br>
+	<div align="center" style="color: red"><?php echo $error ?></div>
+	<form action="register.php" method="post" align="center">
+		<div class="title">Username</div>
+		<input type="text" name="username"></input><br>
+		<div class="title">Password</div>
+		<input type="password" name="password"></input>
+		<div class="title">Confirm Password</div>
+		<input type="password" name="password2"></input><br><br>
 		<input type="submit" value="Submit"></input>
 	</form>
 </body>
