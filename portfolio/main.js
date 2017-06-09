b2e79c721d5ec4455d045cc4abe3a58253938bf6
Gnxela <qawswedfrgtt@gmail.com>
2017-06-09 22:05:09 +0100

    Added simple background image fade in on load. It tried to detect if the image is in cache, and if so don't bother with the fade in. Not a perfect solution, but it works for now.

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
 	colorChange();
 	setInterval(colorChange, 5000);
+
+	var image = new Image();
+	image.src = 'http://www.alexng.me/portfolio/res/frontpage.jpg';
+	if(image.complete) {//Simple cache detection. More testing requited.
+		console.log("Loaded from cache!");
+		$(".frontpage").css({"background-image": "url(" + image.src + ")", "opacity": 1})
+	} else {
+		$(image).on('load', function() {
+			console.log("Loaded image!");
+			$('.frontpage').css("background-image", "url(" + $(this).attr("src") + ")").animate({
+				opacity: 1
+			}, 1000);
+		});
+	}
 });
 function colorChange() {
         var color = "rgb(" + Math.round(Math.random() * 256) + ", " + Math.round(Math.random() * 256) + ", " + Math.round(Math.random() * 256) + ")";
         $('.colorChange').css("color", color);
-	console.log(1);
 }
-
